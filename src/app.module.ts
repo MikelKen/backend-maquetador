52b106a34e21717a41f8c6d8b6b0df48c4efb5d3
Mikel Ken <mayki6866@gmail.com>
2025-04-28 13:20:29 -0400

    feat(puerto):correcion de puerto

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,13 @@ import { CollaborationModule } from './collaboration/collaboration.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT
+        ? parseInt(process.env.DATABASE_PORT)
+        : 5432,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [User, Project, ProjectUser],
       synchronize: true,
     }),
