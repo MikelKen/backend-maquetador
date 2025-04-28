4c8dcad12861c93aaee6a19c1ff8e9b11b9f3c43
Mikel Ken <mayki6866@gmail.com>
2025-04-28 18:45:27 -0400

    feat(url):configuracion de url

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/key.pem'),
+    cert: fs.readFileSync('./secrets/cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
 
